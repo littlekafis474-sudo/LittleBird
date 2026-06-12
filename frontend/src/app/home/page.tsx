5dacba1c02c16eb3a32145735c6709e75fb4efdb
littlekafis474-sudo <littlekafis474@gmail.com>
2026-06-12 02:28:06 +0000

    Add home page

---
+++ frontend/src/app/home/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Welcome to LittleBird! 🐦</h1>
+      <p>Your feed will appear here.</p>
+    </div>
+  );
+}
